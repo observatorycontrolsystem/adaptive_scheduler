f71de971cfb05408f083add11e3f1f48736a862a
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2010-06-28 16:37:48 +0000

    Freezing work on file format client

--- adaptive_scheduler/make_plan_file.py
+++ adaptive_scheduler/make_plan_file.py
@@ -2,6 +2,11 @@
 
 from __future__ import division
 
+# Deprecated. Probably more work than is justified to get something useful.
+
+telescopes = []
+selected_telescope = ''
+
 def new_telescope():
     print "Adding new telescope"
     tel_name  = raw_input("telescope (2m0a): " )
@@ -9,13 +14,27 @@ def new_telescope():
     site_name = raw_input("Site (ogg): ")
 
     telescopes.append(tel_name + '.' + enc_name + '.' + site_name)
+
     
+def select_or_add_telescope():
+    if len(telescopes):
+        print "Existing telescopes:"
+        for n, tel in enumerate(telescopes):
+            print "   (%d) %s" % (n, tel)
+
+    print "   (%d) Add new" % len(telescopes)
+    tel_choice = int(raw_input('[n]> '))
+
+    if tel_choice == len(telescopes):
+        new_telescope()
     
+    global(selected_telescope) = telescopes[tel_choice]
+    print "Selected telescope '%s'" % selected_telescope
+
     
-telescopes = []
-prompt = '[tax]> '
 
 while ( True ):
+    prompt = '%s[tax]> ' % selected_telescope
     choice = raw_input(prompt)
     
     if choice == 'x':
@@ -23,18 +42,5 @@ while ( True ):
         print "Goodbye."
         exit()
 
-    elif choice == 't':
-        if len(telescopes):
-            print "Existing telescopes:"
-            for n, tel in enumerate(telescopes):
-                print "   (%d) %s" % (n, tel)
-
-            print "   (%d) Add new" % len(telescopes)
-            tel_choice = raw_input('[n]> ')
-
-            if tel_choice == len(telescopes):
-                new_telescope()
-            else:
-                selected_tel_idx = tel_choice        
-        else:
-            new_telescope()
+    elif choice == 't':    
+        select_or_add_telescope()
