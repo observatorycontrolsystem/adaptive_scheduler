eec410c62dae1026e67694081bc998ef015f9f7b
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2012-02-09 18:55:47 +0000

    Added metadata support for POND blocks

--- adaptive_scheduler/pond.py
+++ adaptive_scheduler/pond.py
@@ -27,12 +27,41 @@ Author: Eric Saunders
 February 2012
 '''
 
+from adaptive_scheduler.model import DataContainer
 
 
 def ScheduledBlock(object):
-    pass
 
-def Metadata(object):
+    def __init__(self, location, start, end):
+        self.location = location
+
+        # TODO: Extend to allow datetimes or epoch times (and convert transparently)
+        self.start    = start
+        self.end      = end
+
+        self.molecules = []
+
+
+    def is_complete(self):
+        pass
+
+    def add_metadata(self, metadata):
+        self.metadata = metadata
+
+    def add_molecule(self, molecule):
+        # TODO: Handle molecule priorities
+        self.molecules.append(molecule)
+
+    def create_pond_block(self):
+        pass
+
+    def send_to_pond(self):
+        pass
+
+
+
+
+def Metadata(DataContainer):
     pass
 
 def Target(object):
