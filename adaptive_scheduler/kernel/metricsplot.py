ccc95cca29139b2b1d2139d56f9a5ac922c2c6d0
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2012-05-31 21:36:30 +0000

    added metricsplot

---
+++ adaptive_scheduler/kernel/metricsplot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+'''
+MetricsPlot
+
+Author: Sotiria Lampoudi (slampoud@cs.ucsb.edu)
+May 2012
+'''
+
+import matplotlib
+matplotlib.use('Agg')
+from pylab import *
+
+class MetricsPlot(object):
+    
+    def plot_vector(self, v, description, filename):
+        x = []
+        y = []
+        ymax = 0
+        for s, f, val in v:
+            x.append(s)
+            y.append(val)
+            x.append(f)
+            y.append(val)
+            if val > ymax:
+                ymax = val
+        axis([x[0]-1, x[-1]+1, 0, ymax+1])
+        xlabel('time')
+        ylabel('whatever')
+        title(description)
+        plot(x,y)
+        savefig(filename)
