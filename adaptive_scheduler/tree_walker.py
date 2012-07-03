eb1804c68f7a5016d328de7e48aa9d3d469b611e
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2012-07-03 23:17:24 +0000

    unit tests for tree collapse

--- adaptive_scheduler/tree_walker.py
+++ adaptive_scheduler/tree_walker.py
@@ -9,67 +9,116 @@ Author: Eric Saunders
 June 2012
 '''
 
+class TreeCollapser(object):
 
-def is_a_node(node):
-    if 'requests' in node:
+    def __init__(self, input_tree):
+        self.input_tree     = input_tree
+        self.first_time     = True
+        # We assume the tree will collapse, until proven otherwise. Note that a
+        # collapse may not change the tree, if it is already as small as it can be.
+        self.is_collapsible = True
+        self.collapsed_tree = {
+                                'type'     : None,
+                                'requests' : []
+                              }
+
+        return
+
+
+    def validate_node(self, node):
+
+        # Only allow a restricted set of types
+        valid_types = ('single', 'and', 'oneof')
+        if node['type'] not in valid_types:
+            error = "Provided type '%s' is not one of %s." % (
+                                                               node['type'],
+                                                               valid_types
+                                                             )
+            raise InvalidTreeError(error)
+
+        # Singles should only have one child
+        if node['type'] == 'single':
+            n_children = len(self.get_children(node))
+            if n_children != 1:
+                error = "Nodes of type 'single' must have exactly 1 child (got %d)." % (
+                                                                          n_children )
+                raise InvalidTreeError(error)
+
+        # As far as we can tell, it's valid
         return True
 
-    return False
 
-def get_children(node):
-    return node['requests']
+    def is_a_node(self, node):
+        if 'requests' in node:
+            return True
 
-
-node_total = 0
-node_types = set()
-def process_node(node):
-    global node_total
-    global node_types
-    #print "Found a node:", node
-    node_types.add(node['type'])
-    node_total += 1
+        return False
 
 
-leaf_total = 0
-def process_leaf(node):
-    global leaf_total
-    #print "Found a leaf:", node
-    leaf_total += 1
+    def get_children(self, node):
+        return node['requests']
 
 
+    def collapse_node(self, node):
 
-def walk(node, is_a_node, get_children, process_node, process_leaf ):
-
-        if is_a_node(node):
-            process_node(node)
-            for child in get_children(node):
-                walk(child, is_a_node, get_children, process_node, process_leaf)
+        if self.first_time:
+            self.collapsed_tree['type'] = node['type']
+            self.first_time = False
 
         else:
-            process_leaf(node)
-            return
+            if ( node['type'] != self.collapsed_tree['type'] and
+                 node['type'] != 'single' ):
+                # The tree has differing types, and can't be collapsed. Give up.
+               self.is_collapsible = False
 
 
-def collapse_node(node):
-    pass
+    def collapse_leaf(self, node):
+        self.collapsed_tree['requests'].append(node)
 
 
-collapsed_tree = {
-                    'type'     : 'and',
-                    'requests' : []
-                 }
-def collapse_leaf(node):
-    global collapsed_tree
-    collapsed_tree['requests'].append(node)
+    def collapse_tree(self):
+
+        self.collapse(self.input_tree)
+
+        return
+
+
+    def collapse(self, node):
+        # Only continue if we still believe the tree can be collapsed
+        if self.is_collapsible:
+
+            if self.is_a_node(node):
+                self.validate_node(node)
+                self.collapse_node(node)
+
+                # If the tree has now been determined to be uncollapsible, abort.
+                if not self.is_collapsible:
+                    # Reset the tree, which may be partially collapsed
+                    self.collapsed_tree = self.input_tree
+                    return
+
+                # Otherwise, continue collapsing recursively down the tree
+                for child in self.get_children(node):
+                    self.collapse(child)
+
+            # It's a node - deal with it appropriately
+            else:
+                self.collapse_leaf(node)
+                return
 
 
 
+class InvalidTreeError(Exception):
+    '''Raised when the tree is found to be not parseable.'''
 
-def collapse_tree(tree, is_a_node, get_children, collapse_node, collapse_leaf):
+    def __init__(self, value):
+        self.value = value
 
-    walk(tree, is_a_node, get_children, collapse_node, collapse_leaf)
+    def __str__(self):
+        return self.value
 
-    return collapsed_tree
+
+
 
 
 
@@ -79,29 +128,6 @@ if __name__ == '__main__':
     import pprint
 
     pp = pprint.PrettyPrinter()
-
-    tree1 = {
-             'type'     : 'and',
-             'requests' : [
-                              {
-                                'type'     : 'and',
-                                'requests' : [
-                                                 { 'target' : 'blah' },
-                                                 { 'target' : 'blah' },
-                                                 { 'target' : 'blah' },
-                                             ]
-                              },
-                              {
-                                'type'     : 'and',
-                                'requests' : [
-                                                 { 'target' : 'blah' },
-                                                 { 'target' : 'blah' },
-                                                 { 'target' : 'blah' },
-                                             ]
-                              },
-                          ]
-           }
-
 
     tree2 = {
              'type'     : 'and',
--- test/test_tree_walker.py
+++ test/test_tree_walker.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from __future__ import division
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, raises
 
 
-from adaptive_scheduler.tree_walker import ( collapse_tree, is_a_node, get_children,
-                                             collapse_node, collapse_leaf )
+from adaptive_scheduler.tree_walker import TreeCollapser, InvalidTreeError
 
 
 class TestTreeCollapse(object):
@@ -49,6 +48,186 @@ class TestTreeCollapse(object):
                                        ]
                         }
 
-        received_tree = collapse_tree(input_tree, is_a_node, get_children,
-                                      collapse_node, collapse_leaf)
-        assert_equal(expected_tree, received_tree)
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+        assert_equal(expected_tree, self.tc.collapsed_tree)
+
+
+    def test_collapses_all_oneofs(self):
+        input_tree = {
+                         'type'     : 'oneof',
+                         'requests' : [
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                      ]
+                     }
+
+        expected_tree = {
+                          'type'     : 'oneof',
+                          'requests' : [
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                       ]
+                        }
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+        assert_equal(expected_tree, self.tc.collapsed_tree)
+
+
+    def test_collapses_ands_and_singles(self):
+        input_tree = {
+                         'type'     : 'and',
+                         'requests' : [
+                                          {
+                                            'type'     : 'and',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'and',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'single',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                      ]
+                     }
+
+        expected_tree = {
+                          'type'     : 'and',
+                          'requests' : [
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                           { 'target' : 'blah'},
+                                       ]
+                        }
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+        assert_equal(expected_tree, self.tc.collapsed_tree)
+
+
+    def test_cant_collapse_ands_and_oneofs(self):
+        input_tree = {
+                         'type'     : 'and',
+                         'requests' : [
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'and',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                      ]
+                     }
+
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+        assert_equal(self.tc.is_collapsible, False)
+        assert_equal(input_tree, self.tc.collapsed_tree)
+
+
+    @raises(InvalidTreeError)
+    def test_rejects_invalid_type(self):
+        input_tree = {
+                       'type' : 'married',
+                       'requests' : []
+                     }
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+
+
+    @raises(InvalidTreeError)
+    def test_rejects_invalid_single_at_top_level(self):
+        input_tree = {
+                         'type'     : 'single',
+                         'requests' : [
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                      ]
+                     }
+
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
+
+
+    @raises(InvalidTreeError)
+    def test_rejects_invalid_single_at_lower_level(self):
+        input_tree = {
+                         'type'     : 'oneof',
+                         'requests' : [
+                                          {
+                                            'type'     : 'single',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                          {
+                                            'type'     : 'oneof',
+                                            'requests' : [
+                                                             { 'target' : 'blah' },
+                                                             { 'target' : 'blah' },
+                                                         ]
+                                          },
+                                      ]
+                     }
+
+
+        self.tc = TreeCollapser(input_tree)
+        self.tc.collapse_tree()
