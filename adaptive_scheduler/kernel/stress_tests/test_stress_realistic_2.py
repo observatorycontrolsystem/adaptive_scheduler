73931b1d61cde0c95e452a77ab04a891a9a71bbd
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2012-09-12 00:01:51 +0000

    fullscheduler_v5 is a sparse implementation

--- adaptive_scheduler/kernel/stress_tests/test_stress_realistic_2.py
+++ adaptive_scheduler/kernel/stress_tests/test_stress_realistic_2.py
@@ -9,7 +9,7 @@ Sept 2011
 
 from nose.tools import assert_equal
 import copy, random
-from adaptive_scheduler.kernel.fullscheduler_v4 import *
+from adaptive_scheduler.kernel.fullscheduler_v5 import *
 from util import *
 
 class TestStress_realistic(object):
@@ -100,7 +100,7 @@ class TestStress_realistic(object):
 	slice_dict = {}
 	slice_dict['1'] = [1,10*60] # 10 min windows
 	slice_dict['2'] = [1,10*60] # 10 min windows
-        fs = FullScheduler_v4(cr_list, gpw, [], slice_dict)
+        fs = FullScheduler_v5(cr_list, gpw, [], slice_dict)
         s = fs.schedule_all()
         u = Util()
         #u.get_coverage_count_plot(s)
