ce20d17f0fbd00bb3729aba7d12103cb0751af1d
mnorbury <mnorbury@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-02-28 14:11:26 +0000

    Add semester_service functions for return start and end times for semesters.

---
+++ adaptive_scheduler/semester_service.py
@@ -0,0 +1,32 @@
+'''
+semester_service.py - Utility functions for getting the semester dates.
+
+description
+
+Authors: Martin Norbury
+February 2013
+'''
+from datetime import datetime
+
+def _timestamp_is_semester_A(dt):
+    return 4 <= dt.month < 10
+
+def get_semester_start(dt=None):
+    dt = dt or datetime.utcnow()
+    year, month, day = dt.year, 4, 1
+    if not _timestamp_is_semester_A(dt):
+        month = 10
+        if dt.month < 10: year -= 1
+    return datetime(year, month, day)
+
+def get_semester_end(dt=None):
+    dt = dt or datetime.utcnow()
+    year, month, day = dt.year, 10, 1
+    if not _timestamp_is_semester_A(dt):
+        month = 4
+        if dt.month >= 10: year += 1
+    return datetime(year, month, day)
+
+def get_semester_block(dt=None):
+    return get_semester_start(dt), get_semester_end(dt)
+
---
+++ test/test_semester_service.py
@@ -0,0 +1,39 @@
+from nose.tools import eq_
+from datetime import datetime
+
+from adaptive_scheduler import semester_service
+
+def test_return_semester_start_when_in_semester_A():
+    now = datetime(2013, 5, 1)
+    semester_start = semester_service.get_semester_start(now)
+    eq_(semester_start, datetime(2013, 4, 1))
+
+def test_return_semester_end_when_in_semester_A():
+    now = datetime(2013,5,1)
+    semester_end = semester_service.get_semester_end(now)
+    eq_(semester_end, datetime(2013, 10, 1))
+
+def test_return_semester_start_when_in_semester_B():
+    now = datetime(2013, 11, 1)
+    semester_start = semester_service.get_semester_start(now)
+    eq_(semester_start, datetime(2013, 10, 1))
+
+    now = datetime(2014, 2, 1)
+    semester_start = semester_service.get_semester_start(now)
+    eq_(semester_start, datetime(2013, 10, 1))
+
+def test_return_semester_end_when_in_semester_B():
+    now = datetime(2013, 11, 1)
+    semester_end = semester_service.get_semester_end(now)
+    eq_(semester_end, datetime(2014, 4, 1))
+
+    now = datetime(2014, 2, 1)
+    semester_end = semester_service.get_semester_end(now)
+    eq_(semester_end, datetime(2014, 4, 1))
+
+def test_return_semester_block_when_in_semester_B():
+    now            = datetime(2013, 2, 28)
+    expected_start = datetime(2012, 10, 1)
+    expected_end   = datetime(2013, 4, 1)
+    semester_block = semester_service.get_semester_block(now)
+    eq_(semester_block, (expected_start, expected_end))
