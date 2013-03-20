a724b28d5fb405cd87b9a7febf34bca249d28d6a
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-03-20 16:31:32 +0000

    added test for hungarianscheduler

---
+++ test/test_hungarianscheduler.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+'''
+test_hungarianscheduler.py
+
+Author: Sotiria Lampoudi
+June 2012
+'''
+
+from nose.tools import assert_equal
+
+#from adaptive_scheduler.kernel.fullscheduler_v2 import *
+from adaptive_scheduler.kernel.hungarianscheduler import *
+
+class TestBipartiteScheduler(object):
+
+    def setup(self):
+        s1 = Intervals([Timepoint(1, 'start'),
+                        Timepoint(2, 'end')]) # 1-2
+
+        s2 = Intervals([Timepoint(2, 'start'),
+                        Timepoint(4, 'end')]) # --2--4
+
+        self.r1 = Reservation_v2(1, 1, 'foo', s1)
+        self.r2 = Reservation_v2(2, 2, 'bar', s2)
+    
+        self.bs = HungarianScheduler([self.r1, self.r2], ['foo', 'bar'])
+
+
+    def test_create(self):
+        assert_equal(self.bs.reservation_list, [self.r1, self.r2])
+        assert_equal(self.bs.resource_list, ['foo', 'bar'])
+        assert_equal(self.bs.scheduled_reservations, [])
+
+
+    def test_get_reservation_by_ID(self):
+        id = self.r1.get_ID()
+        r = self.bs.get_reservation_by_ID(id)
+        assert_equal(r, self.r1)
+
+
+    def test_hash_and_unhash_quantum_start(self):
+        tmp = self.bs.hash_quantum_start('foo', 2, 1)
+        [resource, start, quantum] = self.bs.unhash_quantum_start(tmp)
+        assert_equal(resource, 'foo')
+        assert_equal(start, 2)
+        assert_equal(quantum, 1)
+       
+ 
+    def test_quantize_windows_1(self):
+        qs = self.bs.quantize_windows(self.r1, 1)
+        [resource, start, quantum] = self.bs.unhash_quantum_start(qs[0])
+        assert_equal(resource, 'foo')
+        assert_equal(start, 1)
+        assert_equal(quantum, 1)
+
+
+    def test_quantize_windows_2(self):
+        qs = self.bs.quantize_windows(self.r1, 1)
+        [resource, start, quantum] = self.bs.unhash_quantum_start(qs[0])
+        assert_equal(resource, 'foo')
+        assert_equal(start, 1)
+        assert_equal(quantum, 1)
+        
+
+    def test_schedule(self):
+        bs2 = HungarianScheduler([self.r1], ['foo']) 
+        sr = bs2.schedule()
+        assert_equal(sr, [self.r1])
+
+# TODO: tests to write
+
+# def test_convert_sparse_to_dense_matrix(self):
+# def test_create_constraint_matrix(self):
+# def test_merge_constraints(self):
