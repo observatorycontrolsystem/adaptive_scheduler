5fd62edc1a32852ff827594394b5a66a7b073e65
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-05-02 23:48:32 +0000

    removed old stress tests, unified stress test interface.

---
+++ adaptive_scheduler/kernel/stress_tests/params_1.py
@@ -0,0 +1,12 @@
+class StressTestParams:
+    def __init__(self):
+        self.numresources=1
+        self.tfinal=30*24*60*60 # 1 month
+        self.numreservations = 30*10 # avg 10 per day
+        self.priority_range=[0,0] # flat
+        self.duration_range=[1,8*60*60] # max 8 hrs
+        self.slack_range=[1,60*60] # max 1 hr
+        self.oneof_elt_num_range=[0,0]
+        self.and_elt_num_range=[0,0]
+        self.slice_length = 10*60 # 10 min
+
