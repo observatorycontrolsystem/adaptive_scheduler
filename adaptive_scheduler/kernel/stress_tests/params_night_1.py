3f42ee2e66977e0194095c258e00894cfdfd8df1
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-06-23 00:23:51 +0000

    parameters

--- adaptive_scheduler/kernel/stress_tests/params_night_1.py
+++ adaptive_scheduler/kernel/stress_tests/params_night_1.py
@@ -5,10 +5,10 @@ class StressTestNightParams:
 	self.tfinal = self.numdays*24*60
         self.avg_res_per_night_per_resource = 5
         self.numreservations = self.avg_res_per_night_per_resource * self.numresources * self.numdays
-        self.night_length = 12*60 # 12 hrs in min
+        self.night_length = 8*60 # 8 hrs in min
         self.priority_range=[1,100] 
         self.duration_range=[1,8*60] # max 8 hrs
-        self.slack_range=[0,4*60] # max 4 hr
+        self.slack_range=[0,1*60] # max 1 hr
         self.oneof_elt_num_range=[2,3]
         self.and_elt_num_range=[2,3]
         self.slice_length = 10 # 10 min
