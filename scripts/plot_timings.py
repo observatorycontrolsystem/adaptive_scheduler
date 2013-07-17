4e2af39b2d1f1e1cbf245e06827f4f538d2585bf
mnorbury <mnorbury@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-07-17 15:11:51 +0000

    Minor tweaks to plot layout

--- scripts/plot_timings.py
+++ scripts/plot_timings.py
@@ -19,44 +19,73 @@ import scipy
 
 Measurement = collections.namedtuple('Measurement', 'datetime value')
 
-def print_summary(data_dict, n_errors):
+def print_summary(summary_data, data_dict, n_errors):
     print '{0:<60} {1:<10}\n'.format('Timer','Number')
     for k,v in data_dict.items():
         print '{0:<60} {1:<10}'.format(k,len(v))
 
     print "\n\nParsing problems with %d lines" % n_errors
 
+def extract_column(line, column_index):
+    dt, elements      = parse_line(line)
+    received_requests = int(elements[column_index])
+    measurement       = Measurement(datetime=dt, value=received_requests)
+    return measurement
+
+def parse_line(line):
+    elements  = line.split()
+    dt_string = '%sT%s' % (elements[0], elements[1])
+    dt = dateutil.parse(dt_string)
+    return dt, elements
+
+def parse_summary_data(line, summary_data):
+    if 'Received' in line and 'User Requests from Request DB' in line:
+        measurement   = extract_column(line, 5)
+        received_data = summary_data.setdefault('requests_in',[])
+        received_data.append(measurement)
+    if 'deleted' in line and 'previously scheduled block' in line:
+        measurement   = extract_column(line, 7)
+        deleted_data = summary_data.setdefault('requests_deleted',[])
+        deleted_data.append(measurement)
+    if 'Submitted' in line and 'new blocks to the POND' in line:
+        measurement   = extract_column(line, 5)
+        new_data = summary_data.setdefault('requests_new',[])
+        new_data.append(measurement)
+
+def parse_timer_data(line, timed_funcs):
+    if 'TIMER' in line:
+        dt, elements = parse_line(line)
+
+        package_name    = elements[6].strip('():')
+        timed_func_name = '%s.%s' % (package_name, elements[5])
+        value           = float(elements[7])
+        measurement     = Measurement(datetime=dt, value=value)
+        func_data       = timed_funcs.setdefault(timed_func_name, [])
+        func_data.append(measurement)
+
 def parse_data(filename):
     log_fh = open(filename, 'r')
 
     timed_funcs = {}
+    summary_data= {}
 
     n_errors = 0
     for line in log_fh:
+        try:
+            parse_timer_data(line, timed_funcs)
+        except ValueError, e:
+            n_errors += 1
+            continue
 
-        if 'TIMER' in line:
-            elements  = line.split()
-            dt_string = '%sT%s' % (elements[0], elements[1])
-            try:
-                dt = dateutil.parse(dt_string)
-            except ValueError:
-                n_errors += 1
-                continue
-
-            package_name    = elements[6].strip('():')
-            timed_func_name = '%s.%s' % (package_name, elements[5])
-            value           = float(elements[7])
-            measurement     = Measurement(datetime=dt, value=value)
-            func_data       = timed_funcs.setdefault(timed_func_name, [])
-            func_data.append(measurement)
+        parse_summary_data(line, summary_data)
 
     log_fh.close()
 
-    return timed_funcs, n_errors
+    return summary_data, timed_funcs, n_errors
 
-def plot_all(timed_funcs):
+def plot_all(summary_data, timed_funcs):
     fig = plt.figure()
-    ax  = fig.add_subplot(111)
+    ax  = fig.add_subplot(211)
 
     for key, timing_data in timed_funcs.iteritems():
         x,y   = zip(*timing_data)
@@ -66,8 +95,19 @@ def plot_all(timed_funcs):
     ax.set_title(filename)
     ax.set_xlabel('Timestamp')
     ax.set_ylabel('Duration (s)')
-    ax.legend()
+    ax.legend(prop={'size':6})
     ax.grid()
+
+    ax2 = fig.add_subplot(212)
+
+    for key, request_data in summary_data.iteritems():
+        x, y  = zip(*request_data)
+        ax2.plot(x,y,'.-',label=key)
+    ax2.set_xlabel('Timestamp')
+    ax2.set_ylabel('Number')
+    ax2.legend()
+    ax2.grid()
+
     fig.show(True)
 
 
@@ -81,7 +121,7 @@ def generate_subplot(total=9):
         subplot_list.append((size,size,plot_number))
     return subplot_list
 
-def hist_all(timed_funcs,number_of_bins=50):
+def hist_all(summary_data, timed_funcs,number_of_bins=50):
     number_of_timers = len(timed_funcs.keys())
 
     fig           = plt.figure()
@@ -108,13 +148,13 @@ def hist_all(timed_funcs,number_of_bins=50):
 
 # Parse the data
 filename = sys.argv[1]
-timed_funcs, n_errors = parse_data(filename)
+summary_data, timed_funcs, n_errors = parse_data(filename)
 
 # Print summary
-print_summary(timed_funcs, n_errors)
+print_summary(summary_data, timed_funcs, n_errors)
 
 # Plot timing_data
-plot_all(timed_funcs)
-hist_all(timed_funcs)
+plot_all(summary_data, timed_funcs)
+hist_all(summary_data, timed_funcs)
 
 raw_input('Press a key to continue')
