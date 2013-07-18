b61b298a0bc53cb099add6e215a0436c62a64d32
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-07-18 16:32:41 +0000

    improved memoization scheme

--- adaptive_scheduler/memoize.py
+++ adaptive_scheduler/memoize.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python
+
+'''
+memoize.py - Transparent function and method caching.
+
+description
+
+Author: Eric Saunders
+July 2013
+'''
+
 import functools
 
 def make_hashable(o):
+    if isinstance(o, dict):
+        return hash(repr(sorted(o.iteritems())))
+    else:
+        return hash(repr(o))
 
+
+def make_hashable_old(o):
     if isinstance(o, (set, tuple, list)):
       return hash(frozenset([type(o)] + [make_hashable(e) for e in o]))
 
@@ -22,24 +39,27 @@ def memoize(obj):
         types if provided.
         Adapted from http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize'''
     cache = obj.cache = {}
+    HASH_NO_ARGS   = make_hashable(())
+    HASH_NO_KWARGS = make_hashable({})
 
     @functools.wraps(obj)
     def memoizer(*args, **kwargs):
-        hashable_args   = make_hashable(args)
-        hashable_kwargs = make_hashable(kwargs)
+        if args:
+            hashable_args   = make_hashable(args)
+        else:
+            hashable_args   = HASH_NO_ARGS
+
+        if kwargs:
+            hashable_kwargs = make_hashable(kwargs)
+        else:
+            hashable_kwargs = HASH_NO_KWARGS
+
         key = (hashable_args, hashable_kwargs)
         if key not in cache:
             cache[key] = obj(*args, **kwargs)
         return cache[key]
     return memoizer
 
-
-@memoize
-def fibonacci(n):
-    '''I am the fibonacci docstring.'''
-    if n in (0, 1):
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
 
 
 from types import MethodType
@@ -58,10 +78,16 @@ class CounterFunction(object):
 
 class Memoize(object):
     """Designed to work as function or method decorator """
+    HASH_NO_ARGS   = make_hashable(())
+    HASH_NO_KWARGS = make_hashable({})
+
     def __init__(self, function):
         self.func = function
         self.cache = {}
         self.cached_func = self._memoize(function)
+
+        self.hit_cache    = 0
+        self.missed_cache = 0
 
     def __call__(self, *args, **kw):
         return self.cached_func(*args, **kw)
@@ -72,132 +98,45 @@ class Memoize(object):
     def _memoize(self, obj):
         @functools.wraps(obj)
         def memoizer(*args, **kwargs):
-            hashable_args   = make_hashable(args)
-            hashable_kwargs = make_hashable(kwargs)
+            if args:
+                hashable_args   = make_hashable(args)
+            else:
+                hashable_args   = Memoize.HASH_NO_ARGS
+
+            if kwargs:
+                hashable_kwargs = make_hashable(kwargs)
+            else:
+                hashable_kwargs = Memoize.HASH_NO_KWARGS
             key = (hashable_args, hashable_kwargs)
             if key not in self.cache:
                 self.cache[key] = obj(*args, **kwargs)
+                self.missed_cache += 1
+            else:
+                self.hit_cache += 1
+
             return self.cache[key]
         return memoizer
 
 
+#@memoize
+def fibonacci(n):
+    '''I am the fibonacci docstring.'''
+    if n in (0, 1):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
 
-from nose.tools import assert_equal, assert_not_equal
-from random import random
+#@memoize
+def fibonacci_iterative(n):
+    '''I am the fibonacci docstring.'''
+    if n in (0, 1):
+        return n
 
-class TestMemoize(object):
+    x_first  = 0
+    x_second = 1
 
-    def setup(self):
-        @memoize
-        def target_manipulation(target):
-            '''I am the docstring.'''
-            return target['ra'] + random()
+    for i in range(2,n+1):
+        x_next   = x_first + x_second
+        x_first  = x_second
+        x_second = x_next
 
-        self.func = target_manipulation
-        self.target  = { 'ra' : 4 }
-        self.target2 = { 'ra' : 8 }
-
-
-    def test_new_values_are_cached(self):
-        assert_equal(len(self.func.cache), 0)
-
-        self.func(self.target)
-        self.func(self.target2)
-
-        assert_equal(len(self.func.cache), 2)
-
-
-    def test_new_values_are_not_cached_again(self):
-        assert_equal(len(self.func.cache), 0)
-
-        self.func(self.target)
-        self.func(self.target)
-
-        assert_equal(len(self.func.cache), 1)
-
-
-    def test_caches_keyword_args(self):
-        assert_equal(len(self.func.cache), 0)
-
-        self.func(target=self.target)
-        self.func(target=self.target)
-
-        assert_equal(len(self.func.cache), 1)
-
-
-    def test_docstring_is_preserved_under_decoration(self):
-        assert_equal(self.func.__doc__, 'I am the docstring.')
-
-
-
-class TestHashable(object):
-
-    def setup(self):
-        class o(object):
-            pass
-
-        self.o = o()
-
-
-    def test_can_hash_immutables(self):
-        immutables = (1, 1.1, True, 'a string', (), frozenset())
-        for x in immutables:
-            assert_equal(type(make_hashable(x)), type(1))
-
-
-    def test_can_hash_dict(self):
-        assert_equal(type(make_hashable({})), type(1))
-
-
-    def test_can_hash_object(self):
-        assert_equal(type(make_hashable(self.o)), type(1))
-
-
-    def test_can_hash_dict_of_dicts(self):
-        d = {
-              'a' : {
-                      'b' : 1
-                    }
-            }
-        assert_equal(type(make_hashable(d)), type(1))
-
-
-    def test_different_dict_contents_have_different_hashes(self):
-        d1 = {
-               'a' : {
-                       'b' : 1
-                     },
-               1 : 33
-             }
-        d2 = {
-               'z' : {
-                       'b' : 1
-                     },
-               1 : 33
-             }
-
-        assert_not_equal(make_hashable(d1), make_hashable(d2))
-
-
-    def test_same_dict_contents_have_same_hashes(self):
-        d1 = {
-               'a' : {
-                       'b' : 1
-                     },
-               1 : 33
-             }
-        d2 = {
-               'a' : {
-                       'b' : 1
-                     },
-               1 : 33
-             }
-
-        assert_equal(make_hashable(d1), make_hashable(d2))
-
-
-    def test_equal_content_different_datatypes_dont_match(self):
-        l = []
-        d = {}
-
-        assert_not_equal(make_hashable(l), make_hashable(d))
+    return x_next
