52a6c412fdc1f89bc4278948ffb4f62c19f93209
slampoudi <slampoudi@0648441e-d613-0410-ad03-a2923e89e7a3>
2013-07-22 23:52:26 +0000

    scripts for generating plots for the paper (for_ejor/ejor.txt explains which), and stress tests used in diagnostics.

--- adaptive_scheduler/kernel/stress_tests/test_stress_comparative.py
+++ adaptive_scheduler/kernel/stress_tests/test_stress_comparative.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
 
 '''
-test_stress_night.py
+stresstester.py
 
 Author: Sotiria Lampoudi
 May 2013
 
-This tester makes un-staggered nights on each resource, and the number of 
-reservations is specified per night.
+This tester makes one big global availability window (0,tfinal) on each 
+resource.
 '''
 
-from nose.tools import assert_equal
 import copy, random
-from adaptive_scheduler.kernel.fullscheduler_v5 import FullScheduler_v5 as CurrentScheduler
+from adaptive_scheduler.kernel.fullscheduler_v3 import FullScheduler_v3 as CurrentScheduler
+from adaptive_scheduler.kernel.fullscheduler_v5 import FullScheduler_v5 as CurrentScheduler2
 from util import *
 from time import time
-from params_night_1 import StressTestNightParams
+from params_1 import StressTestParams
 
-class TestStressNights(object):
+class TestStress(object):
 
     def setup(self):
-        sp = StressTestNightParams()
+        sp = StressTestParams()
         self.outputfname = sp.outputfname
         self.numresources = sp.numresources
         self.numreservations = sp.numreservations
-        self.numdays = sp.numdays
         self.tfinal = sp.tfinal
-        self.night_length = sp.night_length
 
         self.min_duration = sp.duration_range[0]
         self.max_duration = sp.duration_range[1]
@@ -35,33 +33,25 @@ class TestStressNights(object):
         self.min_priority = sp.priority_range[0]
         self.max_priority = sp.priority_range[1]
 
+        # prepare global windows
         self.gpw = {}
         self.slice_dict = {}
-        tmp1 = []
-        for i in range(self.numdays):
-            tmp1.append(Timepoint(i*24*60, 'start'))
-            tmp1.append(Timepoint(i*24*60 + self.night_length, 
-                                  'end'))
-        r = str(0)
-        self.gpw[r] = Intervals(tmp1)
-        self.slice_dict[r] = [0,sp.slice_length]
-
-        for i in range(1,self.numresources): 
-            tmp2 = copy.deepcopy(tmp1)
+        for i in range(sp.numresources):
             r = str(i)
-            self.gpw[r] = Intervals(tmp2)
+            self.gpw[r] = Intervals([Timepoint(0, 'start'), 
+                                     Timepoint(self.tfinal, 'end')])
             self.slice_dict[r] = [0,sp.slice_length]
-        
+
         self.no_oneofs = False
         self.no_ands = False
         self.max_restype = 1
-        if sp.oneof_elt_num_range[0] < 2: #== sp.oneof_elt_num_range[1]:
+        if sp.oneof_elt_num_range[0] == sp.oneof_elt_num_range[1]:
             self.no_oneofs = True
         else:
             self.oneof_elt_num_min = sp.oneof_elt_num_range[0] 
             self.oneof_elt_num_max = sp.oneof_elt_num_range[1] 
             self.max_restype += 1
-        if sp.and_elt_num_range[0] < 2: #== sp.and_elt_num_range[1]:
+        if sp.and_elt_num_range[0] == sp.and_elt_num_range[1]:
             self.no_ands = True
         else:
             self.and_elt_num_min = sp.and_elt_num_range[0] 
@@ -70,41 +60,38 @@ class TestStressNights(object):
 
 
     def generate_reservation(self):
-        start_day = random.randint(0, self.numdays-1)
         duration = random.randint(self.min_duration, self.max_duration)
-        start_time = random.randint(0, self.night_length-duration)
+        start_time = random.randint(0, self.tfinal-duration)
         priority = random.randint(self.min_priority, self.max_priority)
         slack = random.randint(self.min_slack, self.max_slack)
-        resource = str(random.randint(0,self.numresources-1))
 
-        start = start_day*24*60 + start_time
-        window = Intervals([Timepoint(start, 'start'),
-                            Timepoint(start+duration+slack, 'end')])
+        resource = str(random.randint(0,self.numresources-1))
+        window = Intervals([Timepoint(start_time, 'start'),
+                            Timepoint(start_time+duration+slack, 'end')])
         reservation = Reservation_v3(priority, duration, {resource: window})
         return reservation
          
 
-    def test_run_1(self, seed=None):
+    def run_1(self, seed=None):
         if seed:
             random.seed(seed)
         cr_list = []
-        for i in range(0, self.numreservations):
+        for i in range(self.numreservations):
             reservation = self.generate_reservation()
             reslist = [reservation]
-            restype = random.randint(1,self.max_restype)
+            restype = random.randint(1, self.max_restype)
+
             if restype == 1:
                 cr = CompoundReservation_v2(reslist)
             elif restype == 2 and not self.no_oneofs:
                 resnum = random.randint(self.oneof_elt_num_min-1, 
                                         self.oneof_elt_num_max-1)
-                
                 for j in range(resnum):
                     reslist.append(self.generate_reservation())
                 cr = CompoundReservation_v2(reslist,'oneof')
             else: 
                 resnum = random.randint(self.and_elt_num_min-1, 
                                         self.and_elt_num_max-1)
-                
                 for j in range(resnum):
                     reslist.append(self.generate_reservation())
                 cr = CompoundReservation_v2(reslist,'and')
@@ -112,13 +99,20 @@ class TestStressNights(object):
             cr_list.append(cr)
 
         tstart =  time()
-        fs = CurrentScheduler(cr_list, self.gpw, [], self.slice_dict)
-        s = fs.schedule_all()
-        elapsed = time() - tstart
+        fs2 = CurrentScheduler2(cr_list, self.gpw, [], self.slice_dict)
+        s2 = fs2.schedule_all()
+        tend = time()
         u = Util()
-        #u.get_coverage_count_plot(s)
+        u.find_overlaps(s2)
+        for r in s2['0']:
+            r.unschedule()
+        fs = CurrentScheduler(cr_list, self.gpw, [])
+        s = fs.schedule_all()
+        tend2 = time()
+        print len(s2['0']), sum(r.priority for r in s2['0']), tend-tstart, len(s['0']), sum(r.priority for r in s['0']), tend2-tend
         u.find_overlaps(s)
-	sched_count = sum(len(s[k]) for k in s.keys())
-	utilization = u.get_total_scheduled_time(s)
-	res_count_in = len(fs.reservation_list)
-	print res_count_in, sched_count, utilization, elapsed
+
+
+    def test_run_many(self, howmany=500):
+        for i in range(howmany):
+            self.run_1()
