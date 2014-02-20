be2726251c25d2982a7dd05b76ebbee6266e09fa
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2014-02-20 18:15:55 +0000

    more cleanup

--- set_paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Set the paths to development versions of needed Python projects
-# Eric Saunders, May 2012
-
-export PYTHONPATH=$HOME/projects/requestdb/shared/src
-export PYTHONPATH=$HOME/programming/python/rise_set:$PYTHONPATH
-export PYTHONPATH=$HOME/projects:$PYTHONPATH
-
-echo "PYTHONPATH set to:" $PYTHONPATH
