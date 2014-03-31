57a63a7223f56f1ac3dbf8dfaaf0e315d173869d
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2014-03-31 20:51:24 +0000

    hackjob on poor old semester_service to handle 2013B extra month

--- adaptive_scheduler/semester_service.py
+++ adaptive_scheduler/semester_service.py
@@ -6,13 +6,29 @@ and semester B from October->April. A semester is assumed to start at
 midnight on April/October 1st and finish at midnight on October/April 1st
 i.e. the start and stop times coincide.
 
+SPECIAL NOTE: In 2014, the semester boundary was shifted by one month, because of
+a delayed start to public operations. Therefore 2013B finishes in May 2014, and
+2014A begins in May 2014, rather than the usual April. This explains the cruftiness
+added to the previous elegant algorithm for these semesters.
+
 Authors: Martin Norbury
+         Eric Saunders
 February 2013
 '''
 from datetime import datetime
 
+def _semester_is_2014A(dt):
+    if dt.year == 2014:
+        if _timestamp_is_semester_A(dt):
+            return True
+
+    return False
+
 def _timestamp_is_semester_A(dt):
-    return 4 <= dt.month < 10
+    start_month = 4
+    if dt.year == 2014:
+        start_month = 5
+    return start_month <= dt.month < 10
 
 def get_semester_start(dt=None):
     ''' Returns the semester start time (datetime)
@@ -22,6 +38,9 @@ def get_semester_start(dt=None):
     '''
     dt = dt or datetime.utcnow()
     year, month, day = dt.year, 4, 1
+    if _semester_is_2014A(dt):
+        month = 5
+
     if not _timestamp_is_semester_A(dt):
         month = 10
         if dt.month < 10: year -= 1
@@ -37,6 +56,8 @@ def get_semester_end(dt=None):
     year, month, day = dt.year, 10, 1
     if not _timestamp_is_semester_A(dt):
         month = 4
+        if get_semester_code(dt) == '2013B':
+            month = 5
         if dt.month >= 10: year += 1
     return datetime(year, month, day)
 
