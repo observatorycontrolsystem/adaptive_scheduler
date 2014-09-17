4c619ffa57ea280813823b2f970161b610c7f85a
esaunders <esaunders@0648441e-d613-0410-ad03-a2923e89e7a3>
2014-09-17 19:03:21 +0000

    added persistence model to network state lookup for LSC's screwy DB values

--- adaptive_scheduler/monitoring/monitors.py
+++ adaptive_scheduler/monitoring/monitors.py
@@ -110,7 +110,7 @@ class ScheduleTimestampMonitor(NetworkStateMonitor):
 
 
     def retrieve_data(self):
-        return get_datum("Site Agent Schedule Timestamp")
+        return get_datum("Site Agent Schedule Timestamp", persistence_model='STATUS')
 
 
     def create_event(self, datum):
