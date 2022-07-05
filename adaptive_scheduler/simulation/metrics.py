25d4c9666c8fd158b1a68a4770953360c444a4fd
Matthew Wong <mwong@lco.global>
2022-07-05 23:09:30 +0000

    added documentation

--- adaptive_scheduler/simulation/metrics.py
+++ adaptive_scheduler/simulation/metrics.py
@@ -13,33 +13,80 @@ log = logging.getLogger('adaptive_scheduler')
 
 def combine_normal_and_rr_requests_by_rg_id(normal_scheduled_requests_by_rg_id,
                                             rr_scheduled_requests_by_rg_id):
-    # this assumes that a request unique to either normal or rr and cannot be in both
-    # otherwise, write a check that excludes duplicates
+    """Combines normal and scheduled request results for aggregation.
+
+    Args:
+        normal_scheduled_requests_by_rg_id (dict): This is the output of
+            SchedulerResult.get_scheduled_requests_by_request_group_id()
+            which is a dictionary formatted as follows:
+            {rg_id1: {request1: request1_data, request2: request2_data},
+             rg_id2: ...}
+        rr_scheduled_requests_by_rg_id (dict): The same format of results but for
+            rapid response scheduler results.
+
+    Returns:
+        combined_scheduled_requests_by_rg_id (dict): Merged dictionaries with duplicate
+            keys being excluded (OR).
+    """
     return normal_scheduled_requests_by_rg_id | rr_scheduled_requests_by_rg_id
 
 
-def total_scheduled_time(combined_scheduled_requests_by_rg_id):
-    # Sums the duration of all scheduled requests
-    # note, not sure if this is gonna be a timedelta or float object
-    # looking at the existing code, it seems to be an integer for the duration in seconds
+def total_scheduled_time(scheduled_requests_by_rg_id):
+    """Aggregates the total scheduled time.
+
+    Args:
+        scheduled_requests_by_rg_id (dict): SchedulerResult.get_scheduled_requests_by_request_group_id() format.
+
+    Returns:
+        total_scheduled_time (int): The total scheduled time in seconds.
+    """
     total_scheduled_time = 0
-    for request_group in combined_scheduled_requests_by_rg_id.values():
+    for request_group in scheduled_requests_by_rg_id.values():
         for request in request_group.values():
             if request.scheduled:
                 total_scheduled_time += request.duration
     return total_scheduled_time
 
 
-def total_scheduled_count(combined_scheduled_requests_by_rg_id):
+def total_scheduled_count(scheduled_requests_by_rg_id):
+    """Counts the number of scheduled requests."""
     counter = 0
-    for request_group in combined_scheduled_requests_by_rg_id.values():
+    for request_group in scheduled_requests_by_rg_id.values():
         for request in request_group.values():
             if request.scheduled:
                 counter += 1
     return counter
+   
+
+def total_unscheduled_count(scheduled_requests_by_rg_id):
+    """Counts the number of unscheduled requests."""
+    counter = 0
+    for request_group in scheduled_requests_by_rg_id.values():
+        for request in request_group.values():
+            if not request.scheduled:
+                counter += 1
+    return counter
+
+
+def percent_of_requests_scheduled(combined_scheduled_requests_by_rg_id):
+    """Simple percentage scheduled calculation."""
+    scheduled_count = total_scheduled_count(combined_scheduled_requests_by_rg_id)
+    unscheduled_count = total_unscheduled_count(combined_scheduled_requests_by_rg_id)
+    return scheduled_count/(scheduled_count + unscheduled_count) * 100
 
 
 def total_available_time(normal_scheduler_result, rr_scheduler_result, scheduler, horizon_days):
+    """Aggregates the total available time, calculated from dark intervals.
+
+    Args:
+        normal_scheduler_result (SchedulerResult): The normal scheduler result.
+        rr_scheduler_result (SchedulerResult): The rapid response scheduler result.
+        scheduler (LCOGTNetworkScheduler): The scheduler object used by the scheduler runner.
+        horizon_days (float): The length of the horizon in days to calculate the metric.
+
+    Returns:
+        total_available_time (float): The dark intervals capped by the horizon.
+    """
     total_available_time = 0
     normal_resources = normal_scheduler_result.resources_scheduled()
     rr_resources = rr_scheduler_result.resources_scheduled()
@@ -52,39 +99,23 @@ def total_available_time(normal_scheduler_result, rr_scheduler_result, scheduler
             available_time = time_in_capped_intervals(dark_intervals, start_time, end_time)
         total_available_time += available_time
     return total_available_time
-          
-
-def total_unscheduled_count(combined_scheduled_requests_by_rg_id):
-    counter = 0
-    for request_group in combined_scheduled_requests_by_rg_id.values():
-        for request in request_group.values():
-            if request.scheduled:
-                counter += 1
-    return counter
 
 
-def percent_of_requests_scheduled(combined_scheduled_requests_by_rg_id):
-    scheduled_count = total_scheduled_count(combined_scheduled_requests_by_rg_id)
-    unscheduled_count = total_unscheduled_count(combined_scheduled_requests_by_rg_id)
-    return scheduled_count/(scheduled_count + unscheduled_count) * 100
+def reservation_data_populator(reservation):
+    """Creates a new data container containing parameters useful in calculating metrics.
 
+    Args:
+        reservation (Reservation_v3): A Reservation object (obtained from the values of Scheduler.schedule).
 
-
-def request_group_data_populator(reservation):
-    # assumes the proposal/requestgroup is in the format from the observation portal API
+    Returns:
+        data (DataContainer): An object with data values of interest as attributes.
+    """
     request_group = reservation.request_group
     proposal = request_group.proposal
     requests = request_group.requests
     # it may be helpful to directly set max_airmass as an attribute of a request itself
-    max_airmass_by_request_id = {}
-    for request in requests:
-        request_id = request.id
-        # assumes the airmass is the same for all configurations in a request
-        # if not we can maybe aggregate with min/max or avg
-        # again this assumes that configurations is a list of dicts matching the API
-        configuration = request.configurations[0]
-        max_airmass = configuration.constraints['max_airmass']
-        max_airmass_by_request_id[request_id] = max_airmass
+    request_id_configurations = {request.id: request.configurations
+                                 for request in requests}
         
     data = DataContainer(
         request_group_id=reservation.request_group.id,
@@ -95,30 +126,35 @@ def request_group_data_populator(reservation):
         ipp_value=reservation.request_group.ipp_value,
         tac_priority=proposal.tac_priority,
         requests=reservation.request_group.requests,
-        max_airmass_by_request=max_airmass_by_request_id,
+        configurations_by_request_id=request_id_configurations,
     )
     return data
 
-# is this function name too long? or is the specificity necessary?
-def populate_binned_data_dict_with_rg_data(data_dict, key, reservation):
-    request_group_id = reservation.request_group.id
-    if not key in data_dict:
-        data_dict[key] = {}
-    request_group_data = request_group_data_populator(reservation)
-    data_dict[key][request_group_id] = request_group_data
+
+def fill_bin_with_reservation_data(data_dict, bin_name, reservation):
+    """Populates bins in a dictionary with the reservation data container. The original
+    dictionary is modified, instead of creating and returning a copy.
+
+    Args:
+        data_dict (dict): Binned data dictionary. Each bin contains a list of DataContainer's.
+        bin_name (str): The name of the bin to create or populate.
+        reservation (Reservation_v3): A Reservation object.
+    """
+    if not bin_name in data_dict:
+        data_dict[bin_name] = []
+    reservation_data = reservation_data_populator(reservation)
+    data_dict[bin_name].append(reservation_data)
 
 
-def bin_scheduler_result_by_effective_priority(schedule):
-    # this is somewhat structured differently to normal_scheduled_requests_by_rg_id
-    # but we can change it to make it consistent if necessary
-    scheduled_requests_by_priority = {}
+def bin_scheduler_result_by_eff_priority(schedule):
+    scheduled_requests_by_eff_priority = {}
     for reservations in schedule.values():
         for reservation in reservations:
-            priority = str(reservation.priority)
-            populate_binned_data_dict_with_rg_data(scheduled_requests_by_priority,
-                                                   priority,
-                                                   reservation)
-    return scheduled_requests_by_priority
+            eff_priority = str(reservation.priority)
+            fill_bin_with_reservation_data(scheduled_requests_by_eff_priority,
+                                           eff_priority,
+                                           reservation)
+    return scheduled_requests_by_eff_priority
 
 
 def bin_scheduler_result_by_tac_priority(schedule):
@@ -127,28 +163,20 @@ def bin_scheduler_result_by_tac_priority(schedule):
         for reservation in reservations:
             proposal = reservation.request_group.proposal
             tac_priority = str(proposal.tac_priority)
-            populate_binned_data_dict_with_rg_data(scheduled_requests_by_tac_priority,
-                                                   tac_priority,
-                                                   reservation)
+            fill_bin_with_reservation_data(scheduled_requests_by_tac_priority,
+                                           tac_priority,
+                                           reservation)
     return scheduled_requests_by_tac_priority
                 
 
 
-def bin_scheduler_result_by_airmass(scheduler_result):
+def bin_scheduler_result_by_airmass_constr(schedule):
     # TODO
     # the airmasses are in a list which is kind of annoying
-    scheduled_requests_by_airmass = {}
-
-
-def cap_scheduler_results_by_effective_horizon(scheduler_result, horizon_length):
-    # need to confirm the time format for scheduled_start before doing anything
-    # but basically this function truncates the scheduler results to only include things
-    # scheduled within a certain period of time and modifies the schedule accordingly
-    for reservations in scheduler_result.values():
+    scheduled_requests_by_airmass_constr = {}
+    for reservations in schedule.values():
         for reservation in reservations:
-            if reservation.scheduled_start: # is after the horizon
-                reservations.remove(reservation)
-    return scheduler_result
+            pass
 
 
 def calculate_best_airmass_vs_scheduled(scheduler_result):
