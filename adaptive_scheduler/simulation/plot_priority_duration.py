22d1dc24f14889ccb076862aa4682f3560409e47
Michael Wu <qingzewu@berkeley.edu>
2022-07-25 09:55:19 -0700

    plot the priority duration scatterplot

--- adaptive_scheduler/simulation/plot_priority_duration.py
+++ adaptive_scheduler/simulation/plot_priority_duration.py
@@ -3,49 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from opensearchpy import OpenSearch
 from plotutils import get_data_from_opensearch
-VARIABLE = ['with-duration','no-duration',]
+VARIABLE = ['no-duration-v2','no-duration-scaled-100-v2']
             # 'with-duration-scaled-100','no-duration','no-duration-scaled-100',]
 
 markers = ["o" , "," ,"v" , "^" , "<", ">"]
-colors = ['r','g','b','c','m', 'y', 'k']
+colors = ['r','b','c','m', 'y', 'k']
 def rand_jitter(arr):
     stdev = .01 * (max(arr) - min(arr))
     return arr + np.random.randn(len(arr)) * stdev
     
 def plot_sched_priority_duration_dotplot():
-    fig = plt.figure(figsize=(20, 10))
-    fig.suptitle(f'1m0 Scheduled requests Distribution of Priority and Duration With Airmass Optimization', fontsize=20)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(28, 12))
+    fig.suptitle(f'1m0 Distribution of Priority and Duration With Airmass Optimization', fontsize=20)
     fig.subplots_adjust(wspace=0.2, hspace=0.2, top=0.9)
-    ax = fig.add_subplot()
     for i, id in enumerate(VARIABLE):
         data = get_data_from_opensearch(f'1m0-optimize-airmass-{id}')
-        ax.scatter(rand_jitter(data['raw_scheduled_priorities']), rand_jitter(data['raw_scheduled_durations']), 
-                   marker = markers[i],c = colors[i], s = 10, label = f'scheduled requests {id}',alpha = 0.5)
-        # ax.scatter(rand_jitter(data['raw_unscheduled_priorities']), rand_jitter(data['raw_unscheduled_durations']), c = default_colors[5*i+2], marker='^',s=15, label = f'unscheduled requests {id}', alpha = 0.7)
-    ax.set_xlabel('Priority')
-    ax.set_ylabel('Request Duration')
-    ax.legend()
-    plt.show(block=False)
-    plt.show()
-
-def plot_unsched_priority_duration_dotplot():
-    fig = plt.figure(figsize=(20, 10))
-    fig.suptitle(f'1m0 Unscheduled requests Distribution of Priority and Duration With Airmass Optimization', fontsize=20)
-    fig.subplots_adjust(wspace=0.2, hspace=0.2, top=0.9)
-    ax = fig.add_subplot()
+        if id in ['no-duration-scaled-100-v2', 'with-duration-scaled-100-v2']:
+            data['raw_scheduled_priorities'] = [(p+35)/4.5 for p in data['raw_scheduled_priorities']]
+        print(id, len(data['raw_scheduled_priorities']), len(data['raw_unscheduled_priorities']))
+        ax1.scatter(rand_jitter(data['raw_scheduled_priorities']), rand_jitter(data['raw_scheduled_durations']), 
+                   marker = markers[i],c = colors[i], s = 10, label = f'scheduled requests {id}',alpha = 0.3)
+    ax1.set_ylim(top=11000)
+    ax1.set_xlabel('Priority')
+    ax1.set_ylabel('Request Duration')
+    ax1.legend()
     for i, id in enumerate(VARIABLE):
         data = get_data_from_opensearch(f'1m0-optimize-airmass-{id}')
-        # ax.scatter(rand_jitter(data['raw_scheduled_priorities']), rand_jitter(data['raw_scheduled_durations']), 
-        #            marker = markers[i],c = colors[i], s = 10, label = f'scheduled requests {id}',alpha = 0.5)
-        ax.scatter(rand_jitter(data['raw_unscheduled_priorities']), rand_jitter(data['raw_unscheduled_durations']),
-                   c =colors[i], marker=markers[i],s=10, label = f'unscheduled requests {id}', alpha = 0.7)
-    ax.set_xlabel('Priority')
-    ax.set_ylabel('Request Duration')
-    ax.legend()
-    plt.show(block=False)
+        if id in ['no-duration-scaled-100-v2', 'with-duration-scaled-100-v2']:
+            data['raw_unscheduled_priorities'] = [(p+35)/4.5 for p in data['raw_unscheduled_priorities']]
+        ax2.scatter(rand_jitter(data['raw_unscheduled_priorities']), rand_jitter(data['raw_unscheduled_durations']),
+                   c =colors[i], marker=markers[i],s=10, label = f'unscheduled requests {id}', alpha = 0.3)
+    ax2.set_ylim(top=11000)
+    ax2.set_xlabel('Priority')
+    ax2.set_ylabel('Request Duration')
+    ax2.legend()
+    plt.show(block = False)
     plt.show()
 
 if __name__ == '__main__':
-    
-    plot_sched_priority_duration_dotplot()
-    plot_unsched_priority_duration_dotplot()
+    # plot_sched_priority_duration_dotplot()
+    data = get_data_from_opensearch(f'1m0-optimize-airmss-no-duration_scaled-100')
+    print(len(data['unscheduled_priority'][0]), len(data['scheduled_priority'][0]))
