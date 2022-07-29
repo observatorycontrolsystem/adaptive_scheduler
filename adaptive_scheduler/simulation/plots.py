c362f32a017d3f253bc71c7e0a16f309c4abb7ac
Michael Wu <qingzewu@berkeley.edu>
2022-07-29 15:37:26 -0700

    plot func

--- adaptive_scheduler/simulation/plots.py
+++ adaptive_scheduler/simulation/plots.py
@@ -19,6 +19,14 @@ airmass_experiment_ids = [
     '1m0-simulation-real-airmass-coeff-1000000-1',
 ]
 
+effective_priority_experiment_ids = [ 
+    '1m0-optimize-airmass-with-duration-v3',
+    '1m0-optimize-airmass-no-duration-v3',
+    '1m0-optimize-airmass-with-duration-scaled-100-v3',
+    '1m0-optimize-airmass-no-duration-scaled-100-v3',
+]
+
+
 plots = [
     Plot(plotfuncs.plot_airmass_difference_histogram,
          '1m Network Airmass Score Distribution for Scheduled Requests',
@@ -26,6 +34,15 @@ plots = [
     Plot(plotfuncs.plot_pct_scheduled_airmass_binned_priority,
          '1m Network Airmass Experiment Percent of Requests Scheduled',
          *airmass_experiment_ids),
+    Plot(plotfuncs.plot_percent_sched_requests_bin_by_priority,
+         '1m0 Network Scheduler Metrics Binned by Priority',
+         *effective_priority_experiment_ids),
+    Plot(plotfuncs.plot_sched_priority_duration_dotplot,
+         '1m0 Distribution of Priority and Duration With Airmass Optimization',
+         *effective_priority_experiment_ids),
+    Plot(plotfuncs.plot_heat_map_priority_duration,
+         '1m0 Network Requests Heatmap With Airmass Optimization',
+         effective_priority_experiment_ids),
 ]
 
 if __name__ == '__main__':
